deca563315364213e01171bdf57b8f1e4991824d
Jari Kolehmainen <jari.kolehmainen@digia.com>
2014-08-19 18:30:30 +0300

    show error if QTC_TOKEN is not set

--- lib/qtc/cli/common.rb
+++ lib/qtc/cli/common.rb
@@ -17,6 +17,8 @@ module Qtc
       end
 
       def platform_client
+        raise ArgumentError.new('QTC_TOKEN environment variable is not set (you can get it from https://console.qtcloudservices.com)') unless ENV['QTC_TOKEN']
+        
         if @platform_client.nil?
           @platform_client = Qtc::Client.new(platform_base_url, {'Authorization' => "Bearer #{ENV['QTC_TOKEN']}"})
         end
