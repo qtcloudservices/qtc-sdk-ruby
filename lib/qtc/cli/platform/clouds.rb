11e52ebf3e63ce9ce1ae34e53395fa8cd20976c3
Jari Kolehmainen <jari.kolehmainen@digia.com>
2014-11-13 17:44:33 +0200

    cleanup

--- lib/qtc/cli/platform/clouds.rb
+++ lib/qtc/cli/platform/clouds.rb
@@ -11,22 +11,5 @@ module Qtc::Cli::Platform
         print color("~ #{account['name']} (#{account['id']})", :bold)
       end
     end
-
-    def login
-      pass = password("Personal Access Token (copy from https://console.qtcloudservices.com/#/user/profile):")
-      inifile['platform']['token'] = pass
-
-      response = platform_client(pass).get('/user/accounts', {}) rescue nil
-      if response
-        inifile.save(filename: ini_filename)
-      else
-        print color('Invalid Personal Access Token', :red)
-      end
-    end
-
-    def logout
-      inifile['platform'].delete('token')
-      inifile.save(filename: ini_filename)
-    end
   end
 end
